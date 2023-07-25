abdca781d1b7d50f69692de2d53ee9daeb5f1dc7
Francisco Sabino <fdasabino@gmail.com>
2023-07-25 14:48:38 +0200

    added base components

---
+++ src/app/components/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class IngredientsModel {
+  public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;
+  }
+}
+
+//   export class IngredientsModel {
+//   constructor(public name: string, public amount: number) {
+//   }
+// }
--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { IngredientsModel } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: IngredientsModel[] = [
+    new IngredientsModel('Apples', 5),
+    new IngredientsModel('Tomatoes', 5),
+    new IngredientsModel('Limes', 5),
+  ];
 }
